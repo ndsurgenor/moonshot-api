d9dac75b93f8e4a764c48a80e1982c6ae83517ed
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-28 23:53:45 +0000

    Create put functionality for user profiles

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,17 +1,21 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
 class UserProfileList(APIView):
-    """ List all user profiles"""
+    """ Lists all user profiles"""
     def get(self, request):
         user_profiles = UserProfile.objects.all()
         serializer = UserProfileSerializer(user_profiles, many=True)
         return Response(serializer.data)
 
 class UserProfileDetail(APIView):
+    """ Displays the details of a single profile """
+    serializer_class = UserProfileSerializer
+
     def get_object(self, pk):
         try:
             user_profile = UserProfile.objects.get(pk=pk)
@@ -23,3 +27,11 @@ class UserProfileDetail(APIView):
         user_profile = self.get_object(pk)
         serializer = UserProfileSerializer(user_profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
