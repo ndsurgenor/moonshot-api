273556dee8081d646d4977c7afc8de7e12e01eea
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-28 22:56:44 +0000

    Add detail view and error handling for User Profiles

--- user_profiles/urls.py
+++ user_profiles/urls.py
@@ -3,4 +3,5 @@ from user_profiles import views
 
 urlpatterns = [
     path('user-profiles/', views.UserProfileList.as_view()),
+    path('user-profiles/<int:pk>/', views.UserProfileDetail.as_view()),
 ]
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,10 +1,25 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
 class UserProfileList(APIView):
+    """ List all user profiles"""
     def get(self, request):
         user_profiles = UserProfile.objects.all()
         serializer = UserProfileSerializer(user_profiles, many=True)
+        return Response(serializer.data)
+
+class UserProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            user_profile = UserProfile.objects.get(pk=pk)
+            return user_profile
+        except UserProfile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile)
         return Response(serializer.data)
