2c12aab6fca300c51a42f9097a6d586b49bc61c5
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-29 00:52:48 +0000

    Add permissions to prevent editing of user profile by non-owners or logged out users

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from moonshot_api.permissions import IsPermittedOrReadOnly
+
 
 class UserProfileList(APIView):
     """ Lists all user profiles"""
@@ -15,19 +17,23 @@ class UserProfileList(APIView):
 class UserProfileDetail(APIView):
     """ Displays the details of a single profile """
     serializer_class = UserProfileSerializer
+    permission_classes = [IsPermittedOrReadOnly]
 
     def get_object(self, pk):
         try:
             user_profile = UserProfile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, user_profile)
             return user_profile
         except UserProfile.DoesNotExist:
             raise Http404
     
+    # GET functionality
     def get(self, request, pk):
         user_profile = self.get_object(pk)
         serializer = UserProfileSerializer(user_profile)
         return Response(serializer.data)
 
+    # PUT functionality
     def put(self, request, pk):
         user_profile = self.get_object(pk)
         serializer = UserProfileSerializer(user_profile, data=request.data)
