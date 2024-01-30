85e8b60d83cee403460c9e1f16b430411ea4ba5e
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-30 21:19:09 +0000

    Refactor code for photos/views.py

--- photos/views.py
+++ photos/views.py
@@ -1,90 +1,25 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from moonshot_api.permissions import IsPermittedOrReadOnly
 from .models import Photo
 from .serializers import PhotoSerializer
-from moonshot_api.permissions import IsPermittedOrReadOnly
 
 
-class PhotoList(APIView):
+class PhotoList(generics.ListCreateAPIView):
     """
-    Lists all uploaded photos
-    """
+    GET and POST functionality for Photos
+    """ 
     serializer_class = PhotoSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Photo.objects.all()
 
-    def get(self, request):
-        photos = Photo.objects.all()
-        serializer = PhotoSerializer(
-            photos,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = PhotoSerializer(
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+    def link_photo_with_user(self, serializer):
+        serializer.save(user=self.request.user)
 
 
-class PhotoDetail(APIView):
+class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Displays the details of an individual photo
+    PUT and DELETE functionality for Photos
     """
     serializer_class = PhotoSerializer
     permission_classes = [IsPermittedOrReadOnly]
-
-    # Check permissions
-    def get_object(self, pk):
-        try:
-            photo = Photo.objects.get(pk=pk)
-            self.check_object_permissions(self.request, photo)
-            return photo
-        except Photo.DoesNotExist:
-            raise Http404
-    
-    # GET functionality
-    def get(self, request, pk):
-        photo = self.get_object(pk)
-        serializer = PhotoSerializer(
-            photo,
-            context={'request': request},
-        )
-        return Response(serializer.data)
-    
-    # PUT functionality
-    def put(self, request, pk):
-        photo = self.get_object(pk)
-        serializer = PhotoSerializer(
-            photo,
-            data=request.data,
-            context={'request': request},
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    # DELETE functionality
-    def delete(self, request, pk):
-        photo = self.get_object(pk)
-        photo.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Photo.objects.all()
