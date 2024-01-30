c3975c32be6c4ea37dd242b0132589be8fe581c6
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-30 21:11:06 +0000

    Refactor code for user_profiles/views.py

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,62 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from moonshot_api.permissions import IsPermittedOrReadOnly
 from .models import UserProfile
 from .serializers import UserProfileSerializer
-from moonshot_api.permissions import IsPermittedOrReadOnly
 
 
-class UserProfileList(APIView):
+class UserProfileList(generics.ListAPIView):
     """
-    Lists all user profiles
-    """
-    def get(self, request):
-        user_profiles = UserProfile.objects.all()
-        serializer = UserProfileSerializer(
-            user_profiles, 
-            many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+    GET functionality for User Profiles
+    """    
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
 
-class UserProfileDetail(APIView):
+class UserProfileDetail(generics.RetrieveUpdateAPIView):
     """
-    Displays the details of a single profile
+    PUT functionality for User Profiles
     """
     serializer_class = UserProfileSerializer
     permission_classes = [IsPermittedOrReadOnly]
-
-    # Check permissions
-    def get_object(self, pk):
-        try:
-            user_profile = UserProfile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, user_profile)
-            return user_profile
-        except UserProfile.DoesNotExist:
-            raise Http404
-    
-    # GET functionality
-    def get(self, request, pk):
-        user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(
-            user_profile,
-            context={'request': request},
-        )
-        return Response(serializer.data)
-
-    # PUT functionality
-    def put(self, request, pk):
-        user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(
-            user_profile,
-            data=request.data,
-            context={'request': request},
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+    queryset = UserProfile.objects.all()
