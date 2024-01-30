802068c123d1e4e960dd1dab6d233d2b8bc44ed0
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-30 20:17:55 +0000

    Add model for Stars

--- stars/models.py
+++ stars/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from photos.models import Photo
+from .variables import STAR_RATINGS
 
-# Create your models here.
+
+class Star(models.Model):
+    """
+    Star model associated with a specific photo and user
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(
+        Photo,
+        on_delete=models.CASCADE,
+        related_name='stars')
+    created_at = models.DateTimeField(auto_now_add=True)
+    value = models.IntegerField(choices=STAR_RATINGS)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'photo']
+
+    def __str__(self):
+        return f'{self.user}{self.photo}: {self.value} star(s)'
---
+++ stars/variables.py
@@ -0,0 +1,8 @@
+# Defines options for star ratings
+
+STAR_RATINGS = [
+    (0, '0'),
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),     
+]
