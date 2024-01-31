204ba7460c36d8c27462de0d06edbc5195968fcd
Nathan Surgenor <ndsurgenor@gmail.com>
2024-01-31 13:42:35 +0000

    Adjust equipment fields on Photo model

--- photos/models.py
+++ photos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from .variables import MAIN_FEATURES
 
+
 class Photo(models.Model):
     """
     Creates a photo model instance connected to a specific user
@@ -9,7 +10,7 @@ class Photo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=127, blank=True)
+    title = models.CharField(max_length=127, blank=False)
     main_feature = models.CharField(
         max_length=127, choices=MAIN_FEATURES
     )
@@ -17,11 +18,17 @@ class Photo(models.Model):
     location = models.CharField(max_length=127, blank=True)
     photo_date = models.DateField()
     photo_time = models.TimeField()
-    equipment_used = models.TextField(blank=True)
+    # Should autpopulate from 'main_lens' field on Equipment Profile model
+    lens_used = models.CharField(max_length=127, blank=True)
+    # Should autpopulate from 'main_camera' field on Equipment Profile model
+    camera_used = models.CharField(max_length=127, blank=True)
+    # Should autpopulate from 'other_equipment' field on Equipment Profile model
+    other_equipment_used = models.TextField(max_length=127, blank=True)
     image = models.ImageField(
         upload_to='images/',
         default = '../default_photo'
     )
+
 
     class Meta:
         ordering = ['-created_at']
