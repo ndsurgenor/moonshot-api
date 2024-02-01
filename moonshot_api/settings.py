b3ade4c1cdb1e92fca0caedc9b8aa78f2bf154e9
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-01 09:28:58 +0000

    Create 2nd superuser (ms_admin2) and Deploy to Heroku

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
