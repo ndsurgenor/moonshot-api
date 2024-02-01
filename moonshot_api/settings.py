a5d588e1f8aa27e00fa29dde38446df1bbcdce4f
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-01 21:54:24 +0000

    Remove DEBUG for deployment

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -60,7 +60,6 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
