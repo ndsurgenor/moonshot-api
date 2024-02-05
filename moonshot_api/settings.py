46dd94ea34f217bce100c5a0de7fb6d351105ad1
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-05 22:10:49 +0000

    Change seetings.py to ensure Authentication is JWT

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -36,12 +36,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    ),
     #-------------------------------
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.SessionAuthentication'
+    # ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8,
