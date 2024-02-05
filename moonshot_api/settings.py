32e1dcfcdc05435726e1e1d93db314920b983ee1
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-05 22:30:08 +0000

    Remove permission classes from settings.py

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -42,9 +42,6 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-    ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8,
