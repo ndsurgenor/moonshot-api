56ac26e7a59e29e8d035ef783bcd4a99d53e2012
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-06 21:27:41 +0000

    Add DEFAULT_PERMISSION_CLASSES to REST_FRAMEWORK

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -44,7 +44,7 @@ REST_FRAMEWORK = {
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
