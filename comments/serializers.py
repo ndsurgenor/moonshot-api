03091158c95703e84c01e9d46264322eba96e4d9
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-12 09:20:08 +0000

    Remove underscores in 'source=..' in comments/serializers.py

--- comments/serializers.py
+++ comments/serializers.py
@@ -8,8 +8,8 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     user = serializers.ReadOnlyField(source='user.username')
     is_owner = serializers.SerializerMethodField()
-    user_id = serializers.ReadOnlyField(source='user.user_profile.id')
-    user_avatar = serializers.ReadOnlyField(source='user.user_profile.avatar.url')
+    user_id = serializers.ReadOnlyField(source='user.userprofile.id')
+    user_avatar = serializers.ReadOnlyField(source='user.userprofile.avatar.url')
 
     
     def get_is_owner(self, obj):
