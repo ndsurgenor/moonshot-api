1a1105b5250c67b0b30b849f8f4df5adf0f3e7aa
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-21 16:20:32 +0000

    Set global date/time format

--- moonshot_api/settings.py
+++ moonshot_api/settings.py
@@ -192,6 +192,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = '%a %d-%m-%Y %H:%M'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
